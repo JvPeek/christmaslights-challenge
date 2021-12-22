86ad12c0f9c2c7540c4cbe63a1eba746ec8da7d4
realSixi <77362594+realSixi@users.noreply.github.com>
2021-12-22 21:30:27 +0100

    * add dotenv config * change var to const/led

---
+++ config.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+
+module.exports =  {
+    ip: process.env.IP,
+    numberOfLeds: parseInt(process.env.NUMBER_OF_LEDS)
+}
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-var e131 = require('e131');
+const e131 = require('e131');
+const config = require('./config')
 
-var client = new e131.Client('192.168.2.99');  // or use a universe
-var packet = client.createPacket(50*3);  // we want 8 RGB (x3) slots
-var slotsData = packet.getSlotsData();
+const client = new e131.Client(config.ip);  // or use a universe
+const packet = client.createPacket(config.numberOfLeds * 3);  // we want 8 RGB (x3) slots
+const slotsData = packet.getSlotsData();
 packet.setSourceName('test E1.31 client');
 packet.setUniverse(0x01);  // make universe number consistent with the client
 packet.setOption(packet.Options.PREVIEW, true);  // don't really change any fixture
 packet.setPriority(packet.DEFAULT_PRIORITY);  // not strictly needed, done automatically
 
 // slotsData is a Buffer view, you can use it directly
-var color = 0;
+let color = 0;
 function cycleColor() {
-  for (var idx=0; idx<slotsData.length; idx++) {
+  for (let idx = 0; idx < slotsData.length; idx++) {
     slotsData[idx] = color % 0xff;
     color = color + 90;
   }
